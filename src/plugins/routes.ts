b909a9cfbeaa2ae6281fc3204bf72b7146738cdd
Ildar Akhmetov <ildar.akhmetov@gmail.com>
2025-07-13 11:42:03 -0700

    Update routes.ts

--- src/plugins/routes.ts
+++ src/plugins/routes.ts
@@ -4,7 +4,8 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify';
  * Routes plugin - registers all route modules
  */
 async function routes(_fastify: FastifyInstance, _options: FastifyPluginOptions): Promise<void> {
-  // ...
+  // something should be here!
+  // maybe we should call a ping endpoint?
 }
 
 export default routes; 
